7d95bdf8cb8fe7a88712c272b6ff32f29e37c3de
Olivier Grisel <ogrisel@nuxeo.com>
2008-10-08 15:17:37 +0200

    forgot to set USE_EXTERNAL_TEST_LDAP_SERVER back to false in previous checkin

--- nuxeo-platform-directory-ldap/src/test/java/org/nuxeo/ecm/directory/ldap/LDAPDirectoryTestCase.java
+++ nuxeo-platform-directory-ldap/src/test/java/org/nuxeo/ecm/directory/ldap/LDAPDirectoryTestCase.java
@@ -47,7 +47,7 @@ public abstract class LDAPDirectoryTestCase extends NXRuntimeTestCase {
 
     // change this flag to use an external LDAP directory instead of the
     // non networked default ApacheDS implementation
-    public boolean USE_EXTERNAL_TEST_LDAP_SERVER = true;
+    public boolean USE_EXTERNAL_TEST_LDAP_SERVER = false;
 
     // change this flag if your test server has support for dynamic groups
     // through the groupOfURLs objectclass, eg for OpenLDAP:
