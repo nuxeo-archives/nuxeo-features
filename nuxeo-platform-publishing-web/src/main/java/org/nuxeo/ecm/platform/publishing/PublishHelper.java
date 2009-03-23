c3a33ce1366d6db1eb037ebb6dcaf52b2d383053
Julien Thimonier <jthimonier@nuxeo.com>
2009-03-23 11:22:44 +0100

    Just forgot to add this class to the previous commit

---
+++ nuxeo-platform-publishing-web/src/main/java/org/nuxeo/ecm/platform/publishing/PublishHelper.java
@@ -0,0 +1,42 @@
+package org.nuxeo.ecm.platform.publishing;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.platform.ec.notification.service.NotificationServiceHelper;
+import org.nuxeo.ecm.platform.url.DocumentLocationImpl;
+import org.nuxeo.ecm.platform.url.DocumentViewImpl;
+import org.nuxeo.ecm.platform.url.api.DocumentLocation;
+import org.nuxeo.ecm.platform.url.api.DocumentView;
+import org.nuxeo.ecm.platform.url.api.DocumentViewCodecManager;
+import org.nuxeo.runtime.api.Framework;
+
+public final class PublishHelper {
+	
+	private static final Log log = LogFactory.getLog(PublishingActionsListenerBean.class);
+	private static DocumentViewCodecManager docLocator;
+	
+	public static String getUrlFromDocument(DocumentModel doc)
+    {
+    	DocumentLocation docLoc = new DocumentLocationImpl(
+                doc.getRepositoryName(), doc.getRef());
+        DocumentView docView = new DocumentViewImpl(docLoc);
+        docView.setViewId("view_documents");
+        return getDocLocator().getUrlFromDocumentView(
+                        docView,
+                        true,
+                        NotificationServiceHelper.getNotificationService().getServerUrlPrefix());
+    }
+    
+    private static DocumentViewCodecManager getDocLocator() {
+        if (docLocator == null) {
+            try {
+                docLocator = Framework.getService(DocumentViewCodecManager.class);
+            } catch (Exception e) {
+                log.info("Could not get service for document view manager");
+            }
+        }
+
+        return docLocator;
+    }
+}
