4c4e5f6b14fef56d0d6ff9031b28585181eb6d90
Radu Darlea <rdarlea@nuxeo.com>
2009-04-24 11:42:04 +0300

    WEB-241: more on adding action fragment - after a second answer from JMO

--- nuxeo-webengine-sites/src/main/java/org/nuxeo/webengine/sites/models/ContextualLinkListModel.java
+++ nuxeo-webengine-sites/src/main/java/org/nuxeo/webengine/sites/models/ContextualLinkListModel.java
@@ -3,5 +3,7 @@ package org.nuxeo.webengine.sites.models;
 import org.nuxeo.theme.models.AbstractModel;
 
 public class ContextualLinkListModel extends AbstractModel {
+    
+    public ContextualLinkListModel(){}
 
 }
