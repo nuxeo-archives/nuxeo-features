097e7abe1f65c7229db6019c978fd20632570a93
Thierry Delprat <tdelprat@nuxeo.com>
2014-03-25 02:13:27 +0100

    NXP-14013 small - cleanup

--- nuxeo-elastic-search-core/src/main/java/org/nuxeo/elasticsearch/query/AbstractNuxeoESQueryBuilder.java
+++ nuxeo-elastic-search-core/src/main/java/org/nuxeo/elasticsearch/query/AbstractNuxeoESQueryBuilder.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Nuxeo
+ */
+
 package org.nuxeo.elasticsearch.query;
 
 import java.security.Principal;
@@ -9,24 +26,26 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.index.query.BaseQueryBuilder;
 import org.elasticsearch.index.query.BoolFilterBuilder;
 import org.elasticsearch.index.query.FilterBuilders;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.TermsFilterBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.nuxeo.ecm.core.api.ClientException;
-import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 import org.nuxeo.ecm.core.api.SortInfo;
 import org.nuxeo.ecm.core.security.SecurityService;
 import org.nuxeo.elasticsearch.ElasticSearchComponent;
 
+/**
+ *
+ * Base class for generating a SearchRequestBuilder
+ *
+ * @author <a href="mailto:tdelprat@nuxeo.com">Tiry</a>
+ *
+ */
 public abstract class AbstractNuxeoESQueryBuilder {
 
     public TermsFilterBuilder getSecurityFilter(
             final Principal principal) {
-        if (true) {
-            return null;
-        }
         if (principal != null) {
             if (principal instanceof NuxeoPrincipal) {
                 if (((NuxeoPrincipal)principal).isAdministrator()) {
